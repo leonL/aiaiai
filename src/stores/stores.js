7aaf3cc0faf6a0343413ec0eec5b1e7fd80d3680
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-05-06 14:27:54 -0400

    add pi to 1000 digits to store

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,4 +1,6 @@
 import { readable } from 'svelte/store';
 import verses from './verses.js'
+import piDigits from './pi.js'
 
 export const verseStore = readable(verses);
+export const piStore = readable(piDigits)
