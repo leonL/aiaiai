9a5e524dbb4dd1a8d5d58543498736f210a3b8ce
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-05-17 12:13:22 -0400

    letters and numbers emanate from the same location

--- src/Verse.svelte
+++ src/Verse.svelte
@@ -1,51 +1,51 @@
 <script>
-  import { onMount } from 'svelte';
-  import { crossfade } from 'svelte/transition';
+import { onMount } from 'svelte';
+import { crossfade } from 'svelte/transition';
 
-  const [send, receive] = crossfade({duration: 300, delay: 500});
+const [send, receive] = crossfade({duration: 300, delay: 500});
 
-  import Fullstop from './Fullstop.svelte';
+import Fullstop from './Fullstop.svelte';
 
-	export let lineA;
-  export let lineB;
-  export let piId;
+export let lineA;
+export let lineB;
+export let piId;
 
-  const aChars = lineA.split(''),
-    bChars = lineB.split('');
+const aChars = lineA.split(''),
+  bChars = lineB.split('');
 
-  let progLineA = "", wipeCompleted = false, expansionCompleted = false;
+let progLineA = "", wipeCompleted = false, expansionCompleted = false, writeOn = false;
 
-  onMount(async () => {
-    let lineProgressInterval = setInterval(() => {
-			let lineLength = progLineA.length;
-      if (lineLength < lineA.length) {
-        progLineA = progLineA + aChars[progLineA.length];
-      } else {
-        progLineA = progLineA + ' ';
-        clearInterval(lineProgressInterval);
-      };
-		}, 500);
-	});
+function startWriting() {
+  writeOn = true;
+  let lineProgressInterval = setInterval(() => {
+    let lineLength = progLineA.length;
+    if (lineLength < lineA.length) {
+      progLineA = progLineA + aChars[progLineA.length];
+    } else {
+      progLineA = progLineA + ' ';
+      clearInterval(lineProgressInterval);
+    };
+  }, 500);
+};
+
 </script>
 
 <div class='verse'>
-  {#if wipeCompleted}
-  <div class='piCountdown' in:receive={{key: piId}}>{piId}</div>
-  {/if}
-  <div class='couplet'>
-    {#if false}
-    <div class='line-a'>
-      <span class='headless'>{progLineA.substr(0, progLineA.length - 1)}</span>
-      <div class='ultimate char'>{progLineA.substr(progLineA.length - 1, 1)}</div>
-    </div>
+  <div class='number'>
+    {#if wipeCompleted}
+      <span in:receive={{key: piId}} on:introend={ startWriting }>{piId}</span>
     {/if}
   </div>
+  <div class='couplet'>
+    <div class='line-a'>{progLineA.substr(0, progLineA.length - 1)}</div>
+  </div>
   <div class='emanation'>
-    {#if expansionCompleted && !wipeCompleted}
-      <span class='char' out:send={{key: piId}}>{piId}</span>
-    {/if}
     <Fullstop on:expansionComplete = { () => expansionCompleted = true } 
       on:wipeComplete = { () => wipeCompleted = true } />
+    {#if expansionCompleted && !wipeCompleted}
+      <span class='piCount' out:send={{key: piId}}>{piId}</span>
+    {/if}
+    <span class='letter'>{progLineA.substr(progLineA.length - 1, 1)}</span>
   </div>
 </div>
 
@@ -53,19 +53,20 @@
   .verse {
     font-family: 'Times New Roman', Times, serif;
     font-size: 1.2em;
+    margin: 0 10px;
+    text-align: right;
   }
-
-  .emanation {
-    height: 200px;
-  }
-  .char {
+  .number {
     font-family: sans-serif;
+  }
+  .emanation {
+    text-align: left;
+    height: 200px;
     font-size: 200px;
     height: 250px;
     color: black;
   }
-
-  .piCountdown {
+  .piCount {
     font-family: sans-serif;
   }
 </style>
