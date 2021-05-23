23994a093e98246389c01f2bdb5d58c52f18e797
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2021-05-23 18:57:34 -0400

    emanate revealed words letter by letter

---
+++ src/Emanation.svelte
@@ -0,0 +1,47 @@
+<script>
+  /* And the words are the letters of the words said, electrically played */
+  import { afterUpdate, createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher()
+
+  export let wordQueue;
+  
+  let theWord = "", letterInterval = false, letterSpan = 0;
+  
+  afterUpdate(() => {
+    if (letterInterval === false && wordQueue.length > 0) {
+      theWord = wordQueue[0];
+      emanateLetterByLetter();
+    }
+	});
+  
+  function emanateLetterByLetter() {
+    letterSpan = 0;
+    dispatch('emanatingTheWord', theWord);
+    letterInterval = setInterval(() => {
+      if (letterSpan < theWord.length) {
+        letterSpan++;
+      } else {
+        clearInterval(letterInterval);
+        letterInterval = false;
+        letterSpan = 0;
+      };
+    }, 1000);
+  };
+</script>
+
+<div class='the-word'>
+  {#each theWord.substr(0, letterSpan) as letter, i (i)}
+    <span class='letter'>
+      {letter}
+    </span>
+  {/each}
+</div>
+
+<style>
+  .the-word {
+    width: 100%;
+    text-align: center;
+    font-size: 12vw;
+  }
+</style>
--- src/Poem.svelte
+++ src/Poem.svelte
@@ -1,30 +1,54 @@
 <script>
 	import {flip} from 'svelte/animate';
 	import amIWhatIam from './data/amIWhatIAm.js';
+
 	import Verse from './Verse.svelte';
+	import Emanation from './Emanation.svelte';
 
 	export let title;
+	
+	let wordQueue = [];
 
 	let verseIndex = 1;
-	$: activeVerses = amIWhatIam.slice(0, verseIndex);
+	$: activeVersesInReverse = amIWhatIam.slice(0, verseIndex).reverse();
+
+	function addWordToQueue(word) {
+		wordQueue = [...wordQueue, word];
+	};
+
+	function removeWordFromQueue(word) {
+		let wordRemoved = wordQueue.shift();
+		if (wordRemoved !== word) {
+			console.warn(`The word removed from the word queue "${wordRemoved}" is not the expected one "${word}".`);
+		};
+	} 
 </script>
 
 <svelte:head>
   <title>{title}</title>
 </svelte:head>
 
-<main id='aiwia'>
-	{#each activeVerses.reverse() as verse (verse.verseNumber)}
-		<div animate:flip={{duration: 500}}>
-			<Verse {verse} on:verseRevealed={ () => verseIndex++ } />
-		</div>
-	{/each}
+<main>
+	<div class='aiwia'>
+		{#each activeVersesInReverse as verse (verse.verseNumber)}
+			<div animate:flip={{duration: 500}}>
+				<Verse {verse} on:verseRevealed={ () => verseIndex++ } on:wordRevealed= { event => addWordToQueue(event.detail) } />
+			</div>
+		{/each}
+	</div>
+	<Emanation {wordQueue} on:emanatingTheWord= { (event) => removeWordFromQueue(event.detail) } />
 </main>
 
 <style>
-	#aiwia {
-		font-size: 4vw;
+	main {
+		height: 100%;
 		font-family: 'EB Garamond', serif;
+		display: flex;
+		flex-direction: column;
+	}
+	.aiwia {
+		font-size: 4vw;
+		flex-grow: 1;
 		display: flex;
 		flex-direction: column-reverse;
 		height: 100%;
